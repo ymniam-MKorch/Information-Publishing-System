7eb9054067e0aa6b0324d86b010652ac637beee8
ymniam-MKorch <1013287904@qq.com>
2017-08-03 15:51:09 +0800

    v1.4

--- page/terminal/terminal.js
+++ page/terminal/terminal.js
@@ -53,27 +53,33 @@
                                     return dataStr;
                                 }
                             }
-                            //文章标题
-                            if (terminalStr.terminalName.indexOf(selectStr) > -1) {
-                                terminalStr["terminalName"] = changeStr(terminalStr.terminalName);
+                            //终端ID
+                            if (terminalStr.ID.indexOf(selectStr) > -1) {
+                                terminalStr["ID"] = changeStr(terminalStr.ID);
                             }
-                            //发布人
-                            if (terminalStr.terminalAuthor.indexOf(selectStr) > -1) {
-                                terminalStr["terminalAuthor"] = changeStr(terminalStr.terminalAuthor);
+                            //终端名称
+                            if (terminalStr.Name.indexOf(selectStr) > -1) {
+                                terminalStr["Name"] = changeStr(terminalStr.Name);
                             }
-                            //审核状态
-                            if (terminalStr.terminalStatus.indexOf(selectStr) > -1) {
-                                terminalStr["terminalStatus"] = changeStr(terminalStr.terminalStatus);
+                            //所有者
+                            if (terminalStr.Author.indexOf(selectStr) > -1) {
+                                terminalStr["Author"] = changeStr(terminalStr.Author);
                             }
-                            //浏览权限
-                            if (terminalStr.terminalLook.indexOf(selectStr) > -1) {
-                                terminalStr["terminalLook"] = changeStr(terminalStr.terminalLook);
+                            //节目
+                            if (terminalStr.Program.indexOf(selectStr) > -1) {
+                                terminalStr["Program"] = changeStr(terminalStr.Program);
                             }
-                            //发布时间
-                            if (terminalStr.terminalTime.indexOf(selectStr) > -1) {
-                                terminalStr["terminalTime"] = changeStr(terminalStr.terminalTime);
+                            //IP
+                            if (terminalStr.IP.indexOf(selectStr) > -1) {
+                                terminalStr["IP"] = changeStr(terminalStr.IP);
                             }
-                            if (terminalStr.terminalName.indexOf(selectStr) > -1 || terminalStr.terminalAuthor.indexOf(selectStr) > -1 || terminalStr.terminalStatus.indexOf(selectStr) > -1 || terminalStr.terminalLook.indexOf(selectStr) > -1 || terminalStr.terminalTime.indexOf(selectStr) > -1) {
+                            //时间
+                            if (terminalStr.Last_Login.indexOf(selectStr) > -1) {
+                                terminalStr["Last_Login"] = changeStr(terminalStr.Last_Login);
+                            }
+                            if (terminalStr.ID.indexOf(selectStr) > -1 || terminalStr.Name.indexOf(selectStr) > -1
+                                || terminalStr.Author.indexOf(selectStr) > -1 || terminalStr.Program.indexOf(selectStr) > -1
+                                || terminalStr.IP.indexOf(selectStr) > -1 || terminalStr.Last_Login.indexOf(selectStr) > -1) {
                                 newArray.push(terminalStr);
                             }
                         }
@@ -109,32 +115,19 @@
         })
     }).resize();
 
-    //推荐文章
-    $(".recommend").click(function () {
-        var $checkbox = $(".terminal_list").find('tbody input[type="checkbox"]:not([name="show"])');
-        if ($checkbox.is(":checked")) {
-            var index = layer.msg('推荐中，请稍候', { icon: 16, time: false, shade: 0.8 });
-            setTimeout(function () {
-                layer.close(index);
-                layer.msg("推荐成功");
-            }, 2000);
-        } else {
-            layer.msg("请选择需要推荐的文章");
-        }
-    })
-
-    //审核文章
-    $(".audit_btn").click(function () {
+    //刷新进度
+    $(".refresh_all").click(function () {
         var $checkbox = $('.terminal_list tbody input[type="checkbox"][name="checked"]');
         var $checked = $('.terminal_list tbody input[type="checkbox"][name="checked"]:checked');
         if ($checkbox.is(":checked")) {
-            var index = layer.msg('审核中，请稍候', { icon: 16, time: false, shade: 0.8 });
+            var index = layer.msg('刷新中，请稍候', { icon: 16, time: false, shade: 0.8 });
             setTimeout(function () {
                 for (var j = 0; j < $checked.length; j++) {
                     for (var i = 0; i < terminalData.length; i++) {
-                        if (terminalData[i].terminalId == $checked.eq(j).parents("tr").find(".terminal_del").attr("data-id")) {
+                        if (terminalData[i].ID == $checked.eq(j).parents("tr").find(".terminal_del").attr("data-id")) {
                             //修改列表中的文字
-                            $checked.eq(j).parents("tr").find("td:eq(3)").text("审核通过").removeAttr("style");
+                            terminalData[i].Progress = "100%";
+                            terminalList(terminalData);
                             //将选中状态删除
                             $checked.eq(j).parents("tr").find('input[type="checkbox"][name="checked"]').prop("checked", false);
                             form.render();
@@ -142,10 +135,10 @@
                     }
                 }
                 layer.close(index);
-                layer.msg("审核成功");
+                layer.msg("刷新成功");
             }, 2000);
         } else {
-            layer.msg("请选择需要审核的文章");
+            layer.msg("请选择需要刷新的终端");
         }
     })
 
@@ -154,13 +147,13 @@
         var $checkbox = $('.terminal_list tbody input[type="checkbox"][name="checked"]');
         var $checked = $('.terminal_list tbody input[type="checkbox"][name="checked"]:checked');
         if ($checkbox.is(":checked")) {
-            layer.confirm('确定删除选中的信息？', { icon: 3, title: '提示信息' }, function (index) {
+            layer.confirm('确定删除选中的终端？', { icon: 3, title: '提示信息' }, function (index) {
                 var index = layer.msg('删除中，请稍候', { icon: 16, time: false, shade: 0.8 });
                 setTimeout(function () {
                     //删除数据
                     for (var j = 0; j < $checked.length; j++) {
                         for (var i = 0; i < terminalData.length; i++) {
-                            if (terminalData[i].terminalId == $checked.eq(j).parents("tr").find(".terminal_del").attr("data-id")) {
+                            if (terminalData[i].ID == $checked.eq(j).parents("tr").find(".terminal_del").attr("data-id")) {
                                 terminalData.splice(i, 1);
                                 terminalList(terminalData);
                             }
@@ -173,7 +166,7 @@
                 }, 2000);
             })
         } else {
-            layer.msg("请选择需要删除的文章");
+            layer.msg("请选择需要删除的终端");
         }
     })
 
@@ -182,6 +175,7 @@
         var child = $(data.elem).parents('table').find('tbody input[type="checkbox"]:not([name="show"])');
         child.each(function (index, item) {
             item.checked = data.elem.checked;
+            form.render();
         });
         form.render('checkbox');
     });
@@ -198,36 +192,40 @@
         form.render('checkbox');
     })
 
-    //是否展示
-    form.on('switch(isShow)', function (data) {
+    //是否使用
+    form.on('switch(Use)', function (data) {
         var index = layer.msg('修改中，请稍候', { icon: 16, time: false, shade: 0.8 });
         setTimeout(function () {
             layer.close(index);
-            layer.msg("展示状态修改成功！");
+            layer.msg("使用状态修改成功！");
         }, 2000);
     })
 
     //操作
+    $("body").on("click", ".terminal_refresh", function () {  //刷新
+        var _this = $(this);
+        var index = layer.msg('刷新中，请稍候', { icon: 16, time: false, shade: 0.8 });
+        setTimeout(function () {
+            for (var i = 0; i < terminalData.length; i++) {
+                if (terminalData[i].ID == _this.attr("data-id")) {
+                    terminalData[i].Progress = "100%";
+                    terminalList(terminalData);
+                }
+            }
+        layer.close(index);
+        }, 1000);
+    })
+
     $("body").on("click", ".terminal_edit", function () {  //编辑
         layer.alert('您点击了文章编辑按钮，由于是纯静态页面，所以暂时不存在编辑内容，后期会添加，敬请谅解。。。', { icon: 6, title: '文章编辑' });
     })
 
-    $("body").on("click", ".terminal_collect", function () {  //收藏.
-        if ($(this).text().indexOf("已收藏") > 0) {
-            layer.msg("取消收藏成功！");
-            $(this).html("<i class='layui-icon'>&#xe600;</i> 收藏");
-        } else {
-            layer.msg("收藏成功！");
-            $(this).html("<i class='iconfont icon-star'></i> 已收藏");
-        }
-    })
-
     $("body").on("click", ".terminal_del", function () {  //删除
         var _this = $(this);
-        layer.confirm('确定删除此信息？', { icon: 3, title: '提示信息' }, function (index) {
+        layer.confirm('确定删除此终端？', { icon: 3, title: '提示信息' }, function (index) {
             //_this.parents("tr").remove();
             for (var i = 0; i < terminalData.length; i++) {
-                if (terminalData[i].terminalId == _this.attr("data-id")) {
+                if (terminalData[i].ID == _this.attr("data-id")) {
                     terminalData.splice(i, 1);
                     terminalList(terminalData);
                 }
@@ -253,7 +251,8 @@
                         + '<td align="left">' + currData[i].Name + '</td>'
                         + '<td>' + currData[i].Hardware_Identification_Code + '</td>'
                         + '<td>' + currData[i].Author + '</td>'
-                        + '<td>' + currData[i].Progress + '<img src="../../images/refresh.png" alt="刷新" width="35" height="22" align="right" onclick="this.innerHTML=\'谢谢!\'"></td>'
+                        + '<td>' + currData[i].Progress + '<a class="layui-btn layui-btn-mini terminal_refresh" data-id="' + data[i].ID + '" style="background-color:#5FB878;margin-left:30px"><i class="layui-icon">&#x1002;</i></a>'
+                        +'</td>'
                         + '<td>' + currData[i].Program + '</td>';
                     if (currData[i].Status == "1") {
                         dataHtml += '<td><img src="../../images/yes.jpg" alt="是" width="25" height="20"> </td>';
@@ -264,9 +263,15 @@
                         + '<td>' + currData[i].Last_Login + '</td>'
                         + '<td><input type="checkbox" name="show" lay-skin="switch" lay-text="是|否" lay-filter="Use"' + currData[i].Use + '></td>'
                         + '<td>'
-                        + '<a class="layui-btn layui-btn-mini terminal_edit"><i class="iconfont icon-edit"></i> 编辑</a>'
-                        + '<a class="layui-btn layui-btn-normal layui-btn-mini terminal_collect"><i class="layui-icon">&#xe600;</i> 收藏</a>'
-                        + '<a class="layui-btn layui-btn-danger layui-btn-mini terminal_del" data-id="' + data[i].ID + '"><i class="layui-icon">&#xe640;</i> 删除</a>'
+                        + '<div class="btn-group-vertical">'
+                        + '<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">操作<span class="caret"></span>'
+                        + '</button>'
+                        + '<ul class="dropdown-menu">'
+                        + '<li><a class="layui-btn layui-btn-mini terminal_edit"><i class="iconfont icon-edit"></i> 编辑</a></li>'
+                        + '<li><a class="layui-btn layui-btn-normal layui-btn-mini terminal_collect"><i class="layui-icon">&#xe600;</i> 收藏</a></li>'
+                        + '<li><a class="layui-btn layui-btn-danger layui-btn-mini terminal_del" data-id="' + data[i].ID + '"><i class="layui-icon">&#xe640;</i> 删除</a></li>'
+                        + '</ul>'
+                        + '</div>'
                         + '</td>'
                         + '</tr>';
                 }
@@ -277,7 +282,7 @@
         }
 
         //分页
-        var nums = 13; //每页出现的数据量
+        var nums = 10; //每页出现的数据量
         if (that) {
             terminalData = that;
         }
